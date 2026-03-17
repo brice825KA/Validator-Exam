01d260bac6990b04dd6c609f56e470ce14b366f6
brice825KA <brice825KA@users.noreply.github.com>
2026-03-17 09:00:43 +0100

    my_exam_validator

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
         return 84;
-    if (argv[1] == NULL)
+    if (argv[1] == NULL || argv[1][0] == '\0')
         return 84;
     validator(argv[1]);
     return 0;    
